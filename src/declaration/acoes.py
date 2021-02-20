88527053a52c17d43785cce7fcf2e8ddacc02d1d
Leandro Ungari <leandroungari@gmail.com>
2021-02-20 18:59:09 -0300

    fix: corrige geraçao de posiçao quando quantidade igual a zero

--- src/declaration/acoes.py
+++ src/declaration/acoes.py
@@ -10,9 +10,10 @@ class Acoes:
             "\nCódigo: 31\n",
         ]
         for item in self.__data["elementos"]:
-            custo_medio_formatado = f'{"{:.2f}".format(item["precoMedio"])}'.replace('.', ',')
-            custo_total = item["precoMedio"]*item["quantidade"]
-            custo_total_formatado = f'{"{:.2f}".format(custo_total)}'.replace('.', ',')
-            content = f'\n{item["quantidade"]} AÇÕES DE {item["nome"]} CÓDIGO DE NEGOCIAÇÃO B3 "{item["ticker"]}".\nCNPJ {item["cnpj"]}.\nCUSTO TOTAL DE R$ {custo_total_formatado}.\nCUSTO MÉDIO DE R$ {custo_medio_formatado}.\n'
-            result.append(content.upper())
+            if item["quantidade"] > 0 :
+                custo_medio_formatado = f'{"{:.2f}".format(item["precoMedio"])}'.replace('.', ',')
+                custo_total = item["precoMedio"]*item["quantidade"]
+                custo_total_formatado = f'{"{:.2f}".format(custo_total)}'.replace('.', ',')
+                content = f'\n{item["quantidade"]} AÇÕES DE {item["nome"]} CÓDIGO DE NEGOCIAÇÃO B3 "{item["ticker"]}".\nCNPJ {item["cnpj"]}.\nCUSTO TOTAL DE R$ {custo_total_formatado}.\nCUSTO MÉDIO DE R$ {custo_medio_formatado}.\n'
+                result.append(content.upper())
         return result
--- src/declaration/fiis.py
+++ src/declaration/fiis.py
@@ -15,11 +15,12 @@ class Fiis:
             "\nCódigo: 73\n",
         ]
         for item in self.__data["elementos"]:
-            custo_medio_formatado = f'{item["precoMedio"]}'.replace('.', ',')
-            custo_total = item["precoMedio"]*item["quantidade"]
-            custo_total_formatado = f'{"{:.2f}".format(custo_total)}'.replace('.', ',')
-            content = f'\n{item["quantidade"]} COTAS DE {item["nome"]} CÓDIGO DE NEGOCIAÇÃO B3 "{item["ticker"]}".\nCNPJ {item["cnpj"]}.\nCUSTO TOTAL DE R$ {custo_total_formatado}.\nCUSTO MÉDIO DE R$ {custo_medio_formatado}.\n'
-            result.append(content.upper())
+            if item["quantidade"] > 0 :
+                custo_medio_formatado = f'{item["precoMedio"]}'.replace('.', ',')
+                custo_total = item["precoMedio"]*item["quantidade"]
+                custo_total_formatado = f'{"{:.2f}".format(custo_total)}'.replace('.', ',')
+                content = f'\n{item["quantidade"]} COTAS DE {item["nome"]} CÓDIGO DE NEGOCIAÇÃO B3 "{item["ticker"]}".\nCNPJ {item["cnpj"]}.\nCUSTO TOTAL DE R$ {custo_total_formatado}.\nCUSTO MÉDIO DE R$ {custo_medio_formatado}.\n'
+                result.append(content.upper())
         return result
 
     def descreverRendimentos(self):
