7d09fd90e7ec1fe901f46787c10172a73b01c499
Leandro Ungari <leandroungari@gmail.com>
2021-02-20 19:22:02 -0300

    feat: adiciona descriçao de dividendos das açoes

--- src/declaration/acoes.py
+++ src/declaration/acoes.py
@@ -3,6 +3,11 @@ class Acoes:
         self.__data = data
 
     def generate(self):
+        posicoes = self.descreverPosicoes()
+        dividendos = self.descreverDividendos()
+        return posicoes + dividendos
+
+    def descreverPosicoes(self):
         result = [
             "\n==============================",
             "\n==============================",
@@ -16,4 +21,17 @@ class Acoes:
                 custo_total_formatado = f'{"{:.2f}".format(custo_total)}'.replace('.', ',')
                 content = f'\n{item["quantidade"]} AÇÕES DE {item["nome"]} CÓDIGO DE NEGOCIAÇÃO B3 "{item["ticker"]}".\nCNPJ {item["cnpj"]}.\nCUSTO TOTAL DE R$ {custo_total_formatado}.\nCUSTO MÉDIO DE R$ {custo_medio_formatado}.\n'
                 result.append(content.upper())
+        return result
+
+    def descreverDividendos(self):
+        result = [
+            "\n==============================",
+            "\n==============================",
+            "\nRENDIMENTOS ISENTOS E NÃO TRIBUTÁVEIS",
+            "\nCódigo: 26 - Lucros e dividendos recebidos\n",
+        ]
+        for item in self.__data["elementos"]:
+            rendimentos_formatado = f'{"{:.2f}".format(item["dividendos"])}'.replace('.', ',')
+            content = f'\nCNPJ Fonte Pagadora: {item["cnpj"]}\nNome Fonte Pagadora: {item["nome"]}\nValor: R$ {rendimentos_formatado}\n'
+            result.append(content.upper())
         return result
